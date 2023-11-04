17380de63dc2d83e9c373788d32f7832c49fb161
sakibul <sakibul706@gmail.com>
2023-11-04 18:54:14 +0600

    front end :)

--- static/frontend/index.js
+++ static/frontend/index.js
@@ -5,6 +5,7 @@ const ar = document.getElementById("arabic");
 const eng = document.getElementById("english");
 const harakat = document.getElementById("harakats");
 const vocabFormErr = document.getElementById("vocab-form-err-div");
+const editVocabs = document.querySelectorAll("[data-edit]");
 
 
 vocabs.forEach(e => {
@@ -23,6 +24,79 @@ vocabs.forEach(e => {
         ).catch(err => {
             console.error(err);
         });
+    })
+});
+
+editVocabs.forEach(e => {
+    e.addEventListener("click", () => {
+        const id = e.getAttribute("data-edit");
+        const parent = document.getElementById(id);
+        const ar = parent.querySelector(".arabic");
+        const eng = parent.querySelector(".english");
+        const arTxtPre = ar.innerText;
+        const engTxtPre = ar.innerText;
+
+        const keyDown = (e) => {
+            if (e.keyCode == 13) {
+                cleanAndReq()
+            }
+        };
+
+        const cleanAndReq = () => {
+            // cleaning up
+            ar.contentEditable = false;
+            eng.contentEditable = false;
+            parent.replaceChild(e, nd);
+            ar.removeEventListener("keydown", keyDown);
+            eng.removeEventListener("keydown", keyDown);
+
+            let arTxt = ar.innerText;
+            let engTxt = eng.innerText;
+            arTxt = arTxt.trim();
+            engTxt = engTxt.trim();
+
+            if (arTxt.includes("\n")) {
+                console.error("ar text contins new line");
+                return;
+            }
+            if (engTxt.includes("\n")) {
+                console.error("eng text contins new line");
+                return;
+            }
+
+            console.log(arTxt, engTxt);
+        };
+
+
+        ar.addEventListener("keydown", keyDown)
+
+        eng.addEventListener("keydown", keyDown);
+
+        let nd = document.createElement("td");
+        nd.innerText = "submit ->"
+
+        parent.replaceChild(nd, e);
+
+        nd.addEventListener("click", cleanAndReq);
+
+
+        ar.contentEditable = true;
+        eng.contentEditable = true;
+
+
+        // fetch(`/api/remove?id=${id}`, {
+        //     method: "POST",
+        // }).then((res) => {
+        //     if (res.redirected) {
+        //         // console.log(res.headers)
+        //         window.location.href = "/"
+        //     }
+        //     console.log(res)
+        // }
+
+        // ).catch(err => {
+        //     console.error(err);
+        // });
     })
 });
 
