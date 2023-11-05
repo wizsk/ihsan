0803389258e826686477495131a873a44d752f71
sakibul <sakibul706@gmail.com>
2023-11-05 09:04:28 +0600

    more frnd end stuff

--- static/frontend/index.js
+++ static/frontend/index.js
@@ -1,5 +1,6 @@
 // yo js :), Don't remove this comment.
-const vocabs = document.querySelectorAll("[data-remove]");
+// const okIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/> </svg>`;
+const okIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/> </svg>`; const vocabs = document.querySelectorAll("[data-remove]");
 const vocabForm = document.getElementById("vocab-form");
 const ar = document.getElementById("arabic");
 const eng = document.getElementById("english");
@@ -31,10 +32,12 @@ editVocabs.forEach(e => {
     e.addEventListener("click", () => {
         const id = e.getAttribute("data-edit");
         const parent = document.getElementById(id);
+        const icons = parent.querySelector(".icons");
         const ar = parent.querySelector(".arabic");
         const eng = parent.querySelector(".english");
         const arTxtPre = ar.innerText;
-        const engTxtPre = ar.innerText;
+        const engTxtPre = eng.innerText;
+
 
         const keyDown = (e) => {
             if (e.keyCode == 13) {
@@ -43,11 +46,12 @@ editVocabs.forEach(e => {
         };
 
         const cleanAndReq = async () => {
-
             let arTxt = ar.innerText;
             let engTxt = eng.innerText;
             arTxt = arTxt.trim();
             engTxt = engTxt.trim();
+            console.log(ar, eng);
+            console.log("ar", arTxtPre, arTxt, "eng", engTxtPre, engTxt);
 
             if (arTxt.includes("\n")) {
                 console.error("ar text contins new line");
@@ -60,7 +64,7 @@ editVocabs.forEach(e => {
                 return;
             }
 
-            if (arTxtPre === arTxt && engTxtPre === engTxtPre) {
+            if (arTxtPre === arTxt && engTxt === engTxtPre) {
                 console.log("nothing changed");
                 cleanup();
                 return;
@@ -77,12 +81,11 @@ editVocabs.forEach(e => {
                 console.error(res.error)
                 cleanup(true)
                 return;
-            } else if (res.redirected) {
-                window.location.href = "/";
             }
 
             cleanup();
-
+            ar.innerText = arTxt;
+            eng.innerText = engTxt;
         };
 
         // if default is ture then make text to pre.
@@ -91,10 +94,11 @@ editVocabs.forEach(e => {
                 ar.innerText = arTxtPre;
                 eng.innerText = engTxtPre;
             }
+
             // cleaning up
             ar.contentEditable = false;
             eng.contentEditable = false;
-            parent.replaceChild(e, nd);
+            icons.replaceChild(e, nd);
             ar.removeEventListener("keydown", keyDown);
             eng.removeEventListener("keydown", keyDown);
         };
@@ -104,10 +108,11 @@ editVocabs.forEach(e => {
 
         eng.addEventListener("keydown", keyDown);
 
-        let nd = document.createElement("td");
-        nd.innerText = "submit ->"
+        let nd = document.createElement("span");
+        nd.innerHTML = okIcon;
 
-        parent.replaceChild(nd, e);
+        console.log(parent, e, nd)
+        icons.replaceChild(nd, e);
 
         nd.addEventListener("click", cleanAndReq);
 
